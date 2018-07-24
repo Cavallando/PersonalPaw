06e07aa281f064590d5db3aa4a77c0bc7bf775da
Michael Cavallaro <michaelcavallaro03@gmail.com>
2018-07-24 08:02:41 -0600

    Reinit

--- android/app/src/main/java/com/personalpaw/MainApplication.java
+++ android/app/src/main/java/com/personalpaw/MainApplication.java
@@ -3,9 +3,9 @@ package com.personalpaw;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import de.innfactory.apiai.RNApiAiPackage;
 import com.wenkesj.voice.VoicePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import de.innfactory.apiai.RNApiAiPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNApiAiPackage(),
             new VoicePackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNApiAiPackage()
       );
     }
 
--- src/App.js
+++ src/App.js
@@ -5,140 +5,21 @@ import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 
 //imported modules
 import SearchBar from 'react-native-searchbar';
-import Voice from 'react-native-voice';
+import { Dialogflow_V2 } from "react-native-dialogflow"
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: true,
-      searching: false,
-      recognized: '',
-      pitch: '',
-      error: '',
-      end: '',
-      started: '',
-      results: [],
-      partialResults: [],
-    };
-
-    Voice.onSpeechStart = this.onSpeechStart.bind(this);
-    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
-    Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
-    Voice.onSpeechError = this.onSpeechError.bind(this);
-    Voice.onSpeechResults = this.onSpeechResults.bind(this);
-    Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
-    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged.bind(this);
-  }
-
-
-  componentWillUnmount() {
-    Voice.destroy().then(Voice.removeAllListeners);
-  }
-
-  onSpeechStart(e) {
-    this.setState({
-      started: '√',
-    });
-  }
-
-  onSpeechRecognized(e) {
-    this.setState({
-      recognized: '√',
-    });
-  }
-
-  onSpeechEnd(e) {
-    this.setState({
-      end: '√',
-    });
-  }
-
-  onSpeechError(e) {
-    this.setState({
-      error: JSON.stringify(e.error),
-    });
-  }
-
-  onSpeechResults(e) {
-    this.setState({
-      results: e.value,
-    });
-    console.log(e.value);
-    Voice.stop();
-  }
-
-  onSpeechPartialResults(e) {
-    this.setState({
-      partialResults: e.value,
-    });
-  }
-
-  onSpeechVolumeChanged(e) {
-    this.setState({
-      pitch: e.value,
-    });
-  }
-
-  silenceCheck(e) {
-    e._stopRecognizing();
-  }
-  silentVoiceStop(e) {
-    clearTimeout(window.silenceSetTimeout);
-    window.silenceSetTimeout = setTimeout(() => { e.silenceCheck(e) }, 2000);
-  }
-
-  async _startRecognizing(e) {
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: ''
-    });
-    try {
-      await Voice.start('en-US');
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _stopRecognizing(e) {
-    try {
-      await Voice.stop();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _cancelRecognizing(e) {
-    try {
-      await Voice.cancel();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _destroyRecognizer(e) {
-    try {
-      await Voice.destroy();
-    } catch (e) {
-      console.error(e);
-    }
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: ''
-    });
+    Dialogflow_V2.setConfiguration(
+      "client-access@personalpaw.iam.gserviceaccount.com",
+      '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCn6KNqYuGO50Z3\n342LYW7ICapmFmafRiN8iXNZvNnxk2hqN6mLHmhjMp7KxHUsnbJ6D+IE9aog1obi\nnjhfOcNueLHaz5CP8Efm5TORQnClqpmdsnx4mF/U7tpfynhSkmJoWjXWxTZbbBro\n6Y4BCH8/9SfFWSnKxAcwAwjkpkZANVZmclqu3pig7BTZOPuFpyCFOpmWvr16f4Jt\nyGTk7c1/Xx4qrZrmI+plwSJaVIfAhlgWjv5zoNt0M1xxfRSHYAQU11wAk+YXFKl4\nX9QlYyT5/3jO6gU/fwQrear4rclF6Y1nRGn7qz/5GN+m6fL4V8fQO44TuQx2Z+CP\nl5klxr4hAgMBAAECggEABejLm4poWtGr6rsc7atbrUGd1CL0sKVUyWTDAQUC/JwO\nADL+Tg2faeJVsVgxsYTJS790Wm1U8/vo2l/sJvnO9Xvc8pD8ivxPY4Jsiq+2bDYx\nWtn1xH7EXKedWuKy0mEhgn+uRmVBcP1AgzL5EvM8G0GjCTCGpoK4qAlrTjTF4yuO\nxkBh5BQymWP53N5P1556eivYymHUaqImtcQ9RW8Pz6SNeDGyXnV7kmTLBQ6HnlwQ\ni/ruHoiHiEJlFV7QxEGdWuDcVsZkcHjojU7geqtHOudQ3nQQJftnpE86kaToDTcU\nKF/5P+HK3AUkAxqBRbfCtLef4xPfH3QG8H6BwNSYKwKBgQDajVshXMU5mo2Mpjqn\nsjOMOetnHw2omv2jksBEfnb/BPHJsV78SapDzm09C05QGy1KxQ5YwWNjQy3GAlnG\nl9urpIg04315uN1Y9B9313jS4JHtAyOvFUdUerf0hUE0EQu6HehDqFIuo/kFr9uh\nn+5l0vUFn/F+W6dCpRzYTSsPrwKBgQDErdeqeuDbb4ZLSFb8UUeBWojcbKKCM/a0\nofz3wikyZxMGzvk0itgZs8Gen9Hr2f4bBU9bOb5EU6i0tISu/S6XwnlOHJ4yVb3/\nvzICyQfpMWWUepe1jm+q1qyjUa+LQmSVBqPV+/5cjKzci9ZH/1aF14CDmgrneLB6\nyPdeTfEzLwKBgQCBUrvmFfsuhuHTpFl4+d2+0FcXgiyW5H/J1wCmhx7q8IYSjz5h\nk0WJMhE52gLRLAO1Br3ijyy8g/gF/0YYWavG+WkPwr1w1Y9FH8+vHnWEcxZmZUEs\nTS8UMjnAG0nmAWArFZ9myac3qhek4dbY4MY0wovydB52Ys2qhgF3jNI6SQKBgCg9\nrW+rHpCnO+HxViLf+nJj9Las338mZKbGsfx7VHSElGcDOAfhFKAFoGr1Jj1MZ+pd\nZsQyh1RxjYYTnUY0dTEF0E4EGvYPhwVpuDDLsvuqaK89egbissRQkhgEYZdrqSq4\nphLlMUD8Y23oippGiwxtcFT80phToEAvGDXKWrxvAoGBAIStMhi3kt4M/ZeVWqV0\nQImUzUZGj6KjCHyRCzKgsbh5SfcKGV8GuvftffbHL1lS/MHgPxhQ9dCEBL/FKMF4\npR8ybOo1KyRnYKdgYRvIQcHq9YQBVRJ0a24DJhuzZon6E0sioSzm2/qio+NCwVy7\nowA/IhKopQ9tK93e7/0SWgxQ\n-----END PRIVATE KEY-----\n',
+      Dialogflow_V2.LANG_ENGLISH,
+      'personalpaw'
+    );
   }
 
   _onSearchButtonClick(e) {
+    console.log("in here");
     var searching = !this.state.searching;
     if (searching) {
       this.searchBar.show();
@@ -146,7 +27,11 @@ export default class App extends Component {
       this.searchBar.hide();
     }
     this.setState({ searching: searching });
-    this._startRecognizing(e);
+    Dialogflow_V2.startListening(result => {
+      console.log(result);
+    }, error => {
+      console.log(error);
+    })
   }
 
   render() {
@@ -155,7 +40,6 @@ export default class App extends Component {
         <SearchBar
           ref={(ref) => this.searchBar = ref}
           hideX={true}
-          placeholder={this.state.results[0]}
         />
         <Header
           style={styles.header}
@@ -164,13 +48,10 @@ export default class App extends Component {
           }}
         />
         <SearchButton
-          value={this.state.partialResults.map((result, index) => {
-            { result }
-          })}
           style={styles.searchButton}
           iconType="MaterialIcons"
           iconName="mic"
-          onPress={this._onSearchButtonClick.bind(this)}
+          onPress={() => { (e) => this._onSearchButtonClick(e) }}
         />
       </SafeAreaView>
     );

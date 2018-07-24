4164eb9d07bc085914bb391f39f15571975c6d42
Michael Cavallaro <michaelcavallaro03@gmail.com>
2018-07-24 08:10:05 -0600

    Dialogflow working in logs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import { Dialogflow_V2 } from "react-native-dialogflow"
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searching: false,
+    }
     Dialogflow_V2.setConfiguration(
       "client-access@personalpaw.iam.gserviceaccount.com",
       '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCn6KNqYuGO50Z3\n342LYW7ICapmFmafRiN8iXNZvNnxk2hqN6mLHmhjMp7KxHUsnbJ6D+IE9aog1obi\nnjhfOcNueLHaz5CP8Efm5TORQnClqpmdsnx4mF/U7tpfynhSkmJoWjXWxTZbbBro\n6Y4BCH8/9SfFWSnKxAcwAwjkpkZANVZmclqu3pig7BTZOPuFpyCFOpmWvr16f4Jt\nyGTk7c1/Xx4qrZrmI+plwSJaVIfAhlgWjv5zoNt0M1xxfRSHYAQU11wAk+YXFKl4\nX9QlYyT5/3jO6gU/fwQrear4rclF6Y1nRGn7qz/5GN+m6fL4V8fQO44TuQx2Z+CP\nl5klxr4hAgMBAAECggEABejLm4poWtGr6rsc7atbrUGd1CL0sKVUyWTDAQUC/JwO\nADL+Tg2faeJVsVgxsYTJS790Wm1U8/vo2l/sJvnO9Xvc8pD8ivxPY4Jsiq+2bDYx\nWtn1xH7EXKedWuKy0mEhgn+uRmVBcP1AgzL5EvM8G0GjCTCGpoK4qAlrTjTF4yuO\nxkBh5BQymWP53N5P1556eivYymHUaqImtcQ9RW8Pz6SNeDGyXnV7kmTLBQ6HnlwQ\ni/ruHoiHiEJlFV7QxEGdWuDcVsZkcHjojU7geqtHOudQ3nQQJftnpE86kaToDTcU\nKF/5P+HK3AUkAxqBRbfCtLef4xPfH3QG8H6BwNSYKwKBgQDajVshXMU5mo2Mpjqn\nsjOMOetnHw2omv2jksBEfnb/BPHJsV78SapDzm09C05QGy1KxQ5YwWNjQy3GAlnG\nl9urpIg04315uN1Y9B9313jS4JHtAyOvFUdUerf0hUE0EQu6HehDqFIuo/kFr9uh\nn+5l0vUFn/F+W6dCpRzYTSsPrwKBgQDErdeqeuDbb4ZLSFb8UUeBWojcbKKCM/a0\nofz3wikyZxMGzvk0itgZs8Gen9Hr2f4bBU9bOb5EU6i0tISu/S6XwnlOHJ4yVb3/\nvzICyQfpMWWUepe1jm+q1qyjUa+LQmSVBqPV+/5cjKzci9ZH/1aF14CDmgrneLB6\nyPdeTfEzLwKBgQCBUrvmFfsuhuHTpFl4+d2+0FcXgiyW5H/J1wCmhx7q8IYSjz5h\nk0WJMhE52gLRLAO1Br3ijyy8g/gF/0YYWavG+WkPwr1w1Y9FH8+vHnWEcxZmZUEs\nTS8UMjnAG0nmAWArFZ9myac3qhek4dbY4MY0wovydB52Ys2qhgF3jNI6SQKBgCg9\nrW+rHpCnO+HxViLf+nJj9Las338mZKbGsfx7VHSElGcDOAfhFKAFoGr1Jj1MZ+pd\nZsQyh1RxjYYTnUY0dTEF0E4EGvYPhwVpuDDLsvuqaK89egbissRQkhgEYZdrqSq4\nphLlMUD8Y23oippGiwxtcFT80phToEAvGDXKWrxvAoGBAIStMhi3kt4M/ZeVWqV0\nQImUzUZGj6KjCHyRCzKgsbh5SfcKGV8GuvftffbHL1lS/MHgPxhQ9dCEBL/FKMF4\npR8ybOo1KyRnYKdgYRvIQcHq9YQBVRJ0a24DJhuzZon6E0sioSzm2/qio+NCwVy7\nowA/IhKopQ9tK93e7/0SWgxQ\n-----END PRIVATE KEY-----\n',
@@ -51,7 +54,7 @@ export default class App extends Component {
           style={styles.searchButton}
           iconType="MaterialIcons"
           iconName="mic"
-          onPress={() => { (e) => this._onSearchButtonClick(e) }}
+          onPress={this._onSearchButtonClick.bind(this) }
         />
       </SafeAreaView>
     );
